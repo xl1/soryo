56fc52e24e286ec0fa476b9d3d35d45a8698f573
xl1 <shiba200712@gmail.com>
2023-09-01 09:04:35 +0900

    Switch from twitter to twitter-api-v2

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,18 +1,13 @@
 import { Context } from '@azure/functions';
-import Twitter from 'twitter';
+import { TwitterApi } from 'twitter-api-v2';
 import { init, sample } from './sample';
 
 const waitInit = init();
-const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY!,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET!,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY!,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET!,
-});
+const client = new TwitterApi(process.env.TWITTER_BEARER_TOKEN!).v2;
 
 export default async function(context: Context) {
     console.log('invoked');
     const [joyokanji, candidates] = await waitInit;
     const status = sample(joyokanji, candidates);
-    await client.post('statuses/update', { status });
+    await client.tweet(status);
 }
