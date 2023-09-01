9c5e613a1e076c242b004d626b537fefbc27e74f
xl1 <shiba200712@gmail.com>
2023-09-01 09:58:52 +0900

    Update

--- scripts/index.ts
+++ scripts/index.ts
@@ -3,7 +3,12 @@ import { TwitterApi } from 'twitter-api-v2';
 import { init, sample } from './sample';
 
 const waitInit = init();
-const client = new TwitterApi(process.env.TWITTER_BEARER_TOKEN!).v2;
+const client = new TwitterApi({
+    appKey: process.env.TWITTER_CONSUMER_KEY!,
+    appSecret: process.env.TWITTER_CONSUMER_SECRET!,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY!,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET!,
+}).v2;
 
 export default async function(context: Context) {
     console.log('invoked');
